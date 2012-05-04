bcf60cf01493b0ec851d986cba6876776f5c3868
starasov <tarasov.inf@gmail.com>
2012-05-04 08:12:24 -0400

    - More deployment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git pull
 mvn clean package
 
 sudo /etc/init.d/tomcat7 stop
-sudo cp ./highlightr-web/target/highlightr-web.war /var/lib/tomcat7/webapp/ROOT.war
-sudo rm -rf /var/lib/tomcat7/webapp/ROOT/
+sudo cp ./highlightr-web/target/highlightr-web.war /var/lib/tomcat7/webapps/ROOT.war
+sudo rm -rf /var/lib/tomcat7/webapps/ROOT/
 sudo /etc/init.d/tomcat7 start
